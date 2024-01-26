0f1e02a9bcca0f5bb1436fe64996d1631d65df00
Jockey101 <34316135+Jockey12@users.noreply.github.com>
2024-01-26 08:21:49 -0500

    Update passwdGen.cpp

--- passwdGen.cpp
+++ passwdGen.cpp
@@ -1,12 +1,14 @@
+// jockey
+// 1/23/24
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <random>
 
 const int MAX = 90;
-const int MAX1 = 10;
 
 std::string passGen(int n) {
+  
   std::ofstream passwordtext("password.txt");
 
   std::random_device rd;
@@ -15,8 +17,7 @@ std::string passGen(int n) {
   char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U',
     'V','W','X','Y','Z', '0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','(',')'};
-  char numbers[MAX1] = {'1','2','3','4','5','6','7','8','9','0'};
-
+  
   std::string res;
   std::string number;
   std::string combined;
@@ -24,6 +25,7 @@ std::string passGen(int n) {
   if(n <5) {
     std::cout << "Please enter a length of 5 or more to be a strong password.\n";
     return "Try again";
+    
   } else if(n >=5){
     for(int p = 0; p < n; p++) {
       res = res+alphabet[g()%MAX];
@@ -32,6 +34,7 @@ std::string passGen(int n) {
       //Generates random numbers but isnt needed. (If needed Put in combined variable after number+=res >> res+std::to_string(g()) )
     }
   } 
+  
   if(n >110) {
     std::cout << "Please enter a length LESS than or equal to 110\n";
     return "Try Again"; 
@@ -46,7 +49,7 @@ std::string passGen(int n) {
 
 int main () {
   int n;
-  std::cout << "Created by Jockey12, Created 1/23/24\n";
+  
   std::cout << "Version 3\n";
   std::cout << "Welcome to Password Creator\n";
   std::cout << "Length of the password: ";
